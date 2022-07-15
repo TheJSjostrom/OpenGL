373227dfcc42505e115b00263491e9f62c3fd9e7
TheJSjostrom <J.Sjostrom89@gmail.com>
2022-07-15 20:56:18 +0200

    Update

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -22,6 +22,7 @@
 #include "imgui/imgui_impl_glfw_gl3.h"
 
 #include "tests/TestClearColor.h"
+#include "tests/TestTexture2D.h"
 
 int main(void)
 {
@@ -70,6 +71,7 @@ int main(void)
         
         // Adding testse
         testMenu->RegisterTest<test::TestClearColor>("Clear Color");
+        testMenu->RegisterTest<test::TestTexture2D>("2D Texture");
 
         /* Loop until the user closes the window */
         while (!glfwWindowShouldClose(window))
--- OpenGL/src/VertexBufferLayout.h
+++ OpenGL/src/VertexBufferLayout.h
@@ -1,7 +1,10 @@
 #pragma once
+#include <GL/glew.h>
 
 #include <vector>
 #include "Renderer.h"
+
+
 
 struct VertexBufferElement
 {
